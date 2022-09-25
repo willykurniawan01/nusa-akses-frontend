7361570551de3da2c5d64f037e0faa32eb1a583a
Willy kurniawan <willykurniawan50@gmail.com>
2022-09-25 23:12:27 +0700

    fix

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://127.0.0.1:8000";
+const baseUrl = "https://nusa-akses-backend.tiabanautoservice.com";
 const setting = {
   beritaUrl: `${baseUrl}/api/post`,
   servicesUrl: `${baseUrl}/api/services`,
